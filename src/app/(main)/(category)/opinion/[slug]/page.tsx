262282725e5c6103fe574b186db8adfe821dd231
Ashadu Zaman <168162401+ashadu-workmates@users.noreply.github.com>
2025-02-14 12:10:15 +0530

    updated

--- src/app/(main)/(category)/opinion/[slug]/page.tsx
+++ src/app/(main)/(category)/opinion/[slug]/page.tsx
@@ -1,36 +1,73 @@
-import SubPage from '@/components/SubPage'
-import Head from 'next/head'
-import React from 'react'
+import SubPage from "@/components/SubPage";
+import Head from "next/head";
+import React from "react";
 
 const OpinionSubCategory = async ({
-    params,
+  params,
 }: {
-    params: Promise<{ slug: string }>
+  params: Promise<{ slug: string }>;
 }) => {
+  const { slug } = await params;
 
-    const { slug } = await params
+  return (
+    <div>
+      <Head>
+        <meta name="title" content="Nagaland Articles - Latest News" />
+        <meta
+          name="description"
+          content="Explore the latest news and articles about Nagaland."
+        />
+        <meta name="keywords" content="Nagaland, news, articles, updates" />
+        <meta name="author" content="Eastern Mirror" />
+        <meta property="og:title" content="Nagaland Articles - Latest News" />
+        <meta
+          property="og:description"
+          content="Explore the latest news and articles about Nagaland."
+        />
+        <meta
+          property="og:url"
+          content="https://www.easternmirrornagaland.com/nagaland"
+        />
+      </Head>
 
-    return (
-        <div>
-            <Head>
-                <meta name="title" content="Nagaland Articles - Latest News" />
-                <meta name="description" content="Explore the latest news and articles about Nagaland." />
-                <meta name="keywords" content="Nagaland, news, articles, updates" />
-                <meta name="author" content="Eastern Mirror" />
-                <meta property="og:title" content="Nagaland Articles - Latest News" />
-                <meta property="og:description" content="Explore the latest news and articles about Nagaland." />
-                <meta property="og:url" content="https://www.easternmirrornagaland.com/nagaland" />
-            </Head>
+      <SubPage
+        title={
+          slug === "letters-to-editor"
+            ? "Letters to Editor"
+            : slug === "views-and-reviews"
+            ? "Views and Reviews"
+            : slug === "op-ed"
+            ? "Op-Ed"
+            : slug === "book-reviews"
+            ? "Book Reviews"
+            : slug === "editorial"
+            ? "Editorial"
+            : slug
+        }
+        category={slug}
+        links={[
+          {
+            label: "Opinion",
+            href: "/opinion",
+          },
+          {
+            label:
+              slug === "letters-to-editor"
+                ? "Letters to Editor"
+                : slug === "views-and-reviews"
+                ? "Views and Reviews"
+                : slug === "op-ed"
+                ? "Op-Ed"
+                : slug === "book-reviews"
+                ? "Book Reviews"
+                : slug === "editorial"
+                ? "Editorial"
+                : slug,
+          },
+        ]}
+      />
+    </div>
+  );
+};
 
-            <SubPage title={
-                slug === 'letters-to-editor' ? 'Letters to Editor' : slug === 'views-and-reviews' ? 'Views and Reviews' : slug === 'op-ed' ? 'Op-Ed' : slug === 'book-reviews' ? 'Book Reviews' : slug === 'editorial'? 'Editorial': slug} category={slug} links={[{
-                    label: "Opinion",
-                    href: '/opinion'
-                }, {
-                    label: slug === 'letters-to-editor' ? 'Letters to Editor' : slug === 'views-and-reviews' ? 'Views and Reviews' : slug === 'op-ed' ? 'Op-Ed' : slug === 'book-reviews' ? 'Book Reviews': slug === 'editorial'? 'Editorial' : slug
-                }]} />
-        </div>
-    )
-}
-
-export default OpinionSubCategory
+export default OpinionSubCategory;
